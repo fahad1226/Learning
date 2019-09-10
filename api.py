7269edcbfcacf381ea6cf235f2baa007d1dc9386
Fahad Bin Munir <33888412+fahad1226@users.noreply.github.com>
2019-09-10 12:20:50 +0600

    Add files via upload
    Python Stuff

---
+++ api.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "https://www.accuweather.com/"
+
+res = requests.get(url)
+
+print(res.txt)
+print('baal')
---
+++ random.py
@@ -0,0 +1,7 @@
+import random
+
+
+for i in range(5):
+	r = random.random()
+
+print(r)
